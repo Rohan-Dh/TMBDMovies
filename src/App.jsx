f8b11dd932811795240a07a40b9960f7657ac36b
iamdhrohan <rohandhungana2002@gmail.com>
2025-07-29 18:44:01 +0545

    successfully setup bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MovieList from './collections/MovieList.jsx'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import MovieList from "./collections/MovieList.jsx";
+import Navbar from "./collections/Navbar.jsx";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <MovieList/>
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
